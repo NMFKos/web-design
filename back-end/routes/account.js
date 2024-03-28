18669db963cc4f2a186b964d662750771903ff03
double2ci <122618934+leansevenuteen@users.noreply.github.com>
2024-03-29 00:46:02 +0700

    Delete back-end directory

--- back-end/routes/account.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const accountController = require('../app/controllers/AccountController');
-router.use('/', accountController.index)
-
-module.exports = router;
--- back-end/routes/description.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const descriptionController = require('../app/controllers/DescriptionController');
-router.use('/', descriptionController.index)
-
-module.exports = router;
