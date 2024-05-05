758ed93049311b19f52b71f2c106d93bce75708a
double2ci <22520174@gm.uit.edu.vn>
2024-05-05 14:30:45 +0700

    update

--- BackEnd/src/app/controllers/HomeController.js
+++ BackEnd/src/app/controllers/HomeController.js
@@ -1,14 +1,43 @@
 const Posts = require('../modules/post')
 const Images = require('../modules/image')
+const { initializeApp } = require("firebase/app");
+const { getStorage, ref, getDownloadURL, listAll } = require("firebase/storage");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDF36H8mFiTkXTyvRD6z-4YHmqsNCZ4yxE",
+  authDomain: "images-a66c0.firebaseapp.com",
+  projectId: "images-a66c0",
+  storageBucket: "images-a66c0.appspot.com",
+  messagingSenderId: "1081654025998",
+  appId: "1:1081654025998:web:7515c882788d914a3c415f",
+  measurementId: "G-Z6TTKT1H0N"
+};
+
+async function getImage(folderPath) {
+    const app = initializeApp(firebaseConfig);
+    const storage = getStorage(app);
+
+    const listRef = ref(storage, folderPath);
+    const listResult = await listAll(listRef);
+
+    const firstDownloadUrl = await getDownloadURL(listResult.items[0]);
+    return firstDownloadUrl;
+}
 
 class SiteController {
     index(req, res) {
-        Posts.find({})
-        .then(posts => {
+        Posts.find({}).exec()
+        .then(async posts => {
             if (!posts) {
                 throw new Error('404 Not found');
             }
-            const postData = posts.map(p => p.toObject());
+            const postData = posts.slice(30, 40).map(p => p.toObject());
+            // Thêm mã để lấy imagesData cho mỗi bài đăng
+            // for (const post of postData) {
+            //     const folderPath = post.images;
+            //     const imagesData = await getImage(folderPath);
+            //     post.thumbnailData = imagesData;
+            // }
             res.render('home', { showHeader: true, postData });
         })
         .catch(error => {
