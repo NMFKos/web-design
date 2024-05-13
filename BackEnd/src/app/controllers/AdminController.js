f3070d9cd49dec2380bd735ba9523de24d5434d6
peanut <gialacle21@gmail.com>
2024-05-13 10:33:59 +0700

    Add user avt to admin/

--- BackEnd/src/app/controllers/AdminController.js
+++ BackEnd/src/app/controllers/AdminController.js
@@ -1,14 +1,57 @@
+const Post = require('../modules/post');
 const User = require("../modules/user")
+const Stats = require("../modules/stats")
+const mongoose = require('mongoose')
+const { initializeApp } = require('firebase/app')
+const {getStorage, ref, getDownloadURL, listAll} = require('firebase/storage')
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDF36H8mFiTkXTyvRD6z-4YHmqsNCZ4yxE",
+    authDomain: "images-a66c0.firebaseapp.com",
+    projectId: "images-a66c0",
+    storageBucket: "images-a66c0.appspot.com",
+    messagingSenderId: "1081654025998",
+    appId: "1:1081654025998:web:7515c882788d914a3c415f",
+    measurementId: "G-Z6TTKT1H0N"
+};
+
+async function getImageUrls(folderPath) {
+    const app = initializeApp(firebaseConfig);
+    const storage = getStorage(app);
+
+    const listRef = ref(storage, folderPath);
+    const listResult = await listAll(listRef);
+
+    const imageUrls = [];
+    for (const item of listResult.items) {
+        const downloadUrl = await getDownloadURL(item);
+        imageUrls.push(downloadUrl);
+    }
+    return imageUrls
+}
+
+async function getImage(imgPath){
+    const app = initializeApp(firebaseConfig);
+    const storage = getStorage(app);
+    const imageUrl = await getDownloadURL(ref(storage, imgPath));
+    return imageUrl;
+}
 
 class AdminController {
     index(req, res) {
-        User.find({}).exec()
+        let postData;
+        User.find({}).exec() 
         .then(async user=> {
             if(!user){
                 //Nếu không tìm thấy thông tin user
                 throw new Error('404 NOT FOUND');
             }
             const userData = user.slice(30, 42).map(user => user.toObject());
+            for (const user of userData)
+            {
+                const avatarData = await getImage(user.avatar);
+                user.avatar = avatarData;
+            }
             res.render('admin', {showAdmin: true, userData});
         })
         .catch(error => {
@@ -16,6 +59,19 @@ class AdminController {
             res.status(500).send('INTERNAL SERVER ERROR');
         })
     }
+    stats(req, res){
+        Stats.find({}).exec()
+        .then(async stats => {
+            if(!stats){
+                throw new Error('404 NOT FOUND');
+            }
+            const statsData = stats.map(stat => stat.toObject());
+            res.render('admin', {showAdmin: true, statsData});
+        })
+        .catch(error => {
+            console.error('Error fetching stats from database');
+            res.status(500).send('INTERNAL SERVER ERROR');
+        })
+    }
 }
-
 module.exports = new AdminController;
---
+++ BackEnd/src/app/modules/stats.js
@@ -0,0 +1,13 @@
+// const { ObjectId, Int32 } = require("mongodb");
+// const { mongoose } = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const User = new Schema({
+//     _id: { type: ObjectId},
+//     year: {type: Int32},
+//     profits: {type: Int32},
+//     accesses: {type: Int32},
+//     searchs: {type: Int32},
+//   });
+
+// module.exports = mongoose.model('Stats', Stats)
