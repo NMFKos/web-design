a34a3c09a889c5b9b12ef29befedc1793928fb77
peanut <gialacle21@gmail.com>
2024-05-20 12:24:20 +0700

    update home.hdbs

--- BackEnd/src/app/controllers/Chat.js
+++ BackEnd/src/app/controllers/Chat.js
@@ -40,3 +40,57 @@ class InteractiveChatbox {
         }
     }
 }
+const chatHistory = document.getElementById('chatbox-history');
+const userInput = document.getElementById('user-input');
+const form = document.getElementById('chat-form');
+
+async function sendMessage() {
+const userMessage = userInput.value;
+userInput.value = ''; // Clear input field
+console.log(userMessage)
+try {
+    chatHistory.innerHTML += `<div class="user-message">${userMessage}</div>`;
+    const response = await fetch('/chat', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ 'userInput': userMessage }),
+    });
+
+    const data = await response.json();
+    console.log(data)
+    const botMessage = data.response;
+    console.log(botMessage)
+    // Add chat message to the chat history
+    chatHistory.innerHTML += `<div class="bot-message">${botMessage}</div>`;
+
+    // Scroll to the bottom of the chat history
+    chatHistory.scrollTop = chatHistory.scrollHeight;
+} catch (error) {
+    console.error('Error:', error);
+    // Handle errors gracefully, e.g., display an error message to the user
+}
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent form submission
+    const loader = document.getElementById('loader');
+    //loader.style.display = 'none'; // Show the loader
+    sendMessage().finally(() => {
+   // loader.style.display = 'none'; // Hide the loader after the message is sent
+    });;
+});
+
+
+//Open chat button
+const charForm = document.getElementById('chat-form')
+const chatButton = document.querySelector('.chatbox-button');
+const chatContent = document.querySelector('.chatbox-support');
+const icons = {
+    isClicked: '</p>Close</p>',
+    isNotClicked: '<p>Open chatbot</p>'
+}
+const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
+chatbox.display();
+chatbox.toggleIcon(false, chatButton);
