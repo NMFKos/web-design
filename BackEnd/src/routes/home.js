20b29b6acee46685a4c53ddca9dff81d52c6942b
Dương Đạt <144459605+NMFKos@users.noreply.github.com>
2024-05-20 23:44:50 +0700

    Add files via upload

--- BackEnd/src/routes/home.js
+++ BackEnd/src/routes/home.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../app/controllers/HomeController');
+const passport = require('passport')
+const  session = require('express-session');
+const googleAuthDal = require('../app/google-auth');
+require('../public/script/google-auth');
 
 router.post('/dang-nhap', homeController.login);
 router.get('/dang-nhap', homeController.showLogin);
 router.get('/dang-xuat', homeController.logout);
+
 router.get('/can-ho-mot-phong-ngu', (req, res) => {
     homeController.filter(req, res, "1PN");
 });
@@ -19,4 +24,36 @@ router.get('/can-ho-studio', (req, res) => {
 });
 router.get('/bang-gia', homeController.showPriceList);
 router.get('/', homeController.index)
+
+function isLogged(req, res, next){
+    req.user ? next() : res.sendStatus(401);
+}
+
+router.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+
+router.get( '/auth/google/callback',
+passport.authenticate( 'google', {
+    successRedirect: '/auth/google/success',
+    failureRedirect: '/auth/google/failure'
+}));
+
+router.use(session({
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  }));
+
+router.use(passport.initialize());
+router.use(passport.session());
+router.get('/auth/google/success',isLogged,async (req, res)=>{
+    await googleAuthDal.registerWithGoogle(req, res, userProfile);
+    
+})
+router.get('/auth/google/failure',isLogged, (res,req)=>{
+    res.redirect('/dang-nhap')
+})
 module.exports = router;
