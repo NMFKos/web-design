3eec42252caa76f4784acb353bbddd238664742f
Dương Đạt <144459605+NMFKos@users.noreply.github.com>
2024-05-23 14:15:41 +0700

    Add files via upload

--- BackEnd/src/app/google-auth.js
+++ BackEnd/src/app/google-auth.js
@@ -1,30 +1,27 @@
-const User = require('./modules/user');
+const googleUser = require('./modules/google');
 const mongoose = require('mongoose');
 
 const googleAuthDal = {
     registerWithGoogle: async (req, res, oauthUser) => {
         try {
-            let user = await User.findOne({
-                name: oauthUser.displayName,
-                email: oauthUser.emails[0].value,
+            let user = await googleUser.findOne({
+                id: oauthUser._id
             });
 
             if (user) {
                 req.session.userId = user._id;
                 req.session.username = user.name;
-                console.log(user._id);
-                console.log(user.name);
             } else {
-                const newUser = new User({
-                    _id: new mongoose.Types.ObjectId(),
+                const newUser = new googleUser({
+                    id: oauthUser._id,
                     name: oauthUser.displayName,
                     email: oauthUser.emails[0].value, // optional - storing it as extra info
+                    role: 0,
+                    provider: oauthUser.provider
                 });
                 await newUser.save();
                 req.session.userId = newUser._id;
                 req.session.username = newUser.name;
-                console.log(newUser._id);
-                console.log(newUser.name);
             }
 
             return { userId: req.session.userId, username: req.session.username };
